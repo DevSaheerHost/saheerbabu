45fd2e6623e6951ab7e5a67041826e244244b87b
saheer <sheerbabu549@gmail.com>
2025-02-05 12:54:02 +0530

    Portfolio added/Created

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const $_ = selector => document.querySelector(selector)
+/* ==========Typing Animation============ */
+
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Web Developer",
+    "Graphic Designer",
+    "Android App Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+
+/*====Asaid====*/
+const nav = document.querySelector('.nav')
+      navList = nav.querySelectorAll('li')
+      totalNavLis = navList.length;
+      console.log(totalNavLis)
+for (var i = 0; i < totalNavLis.length; i++) {
+        console.log(navList[i])
+        console.log('f')
+      }
